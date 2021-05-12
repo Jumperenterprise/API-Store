5b2f6fdc57b84e17855a3c4602ca7d7220886d7d
Jumperenterprise <andrew.jumper.ent@gmail.com>
2021-05-12 18:12:09 +0200

    first commit

---
+++ src/products/controllers/products.controller.ts
@@ -0,0 +1,73 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Post,
+  Body,
+  Put,
+  Delete,
+  HttpStatus,
+  HttpCode,
+  Res,
+  // ParseIntPipe,
+} from '@nestjs/common';
+
+import { Response } from 'express';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { ParseIntPipe } from '../../common/parse-int.pipe';
+import { CreateProductDto, UpdateProductDto } from '../dtos/products.dtos';
+
+import { ProductsService } from '../services/products.service';
+
+@ApiTags('products')
+@Controller('products')
+export class ProductsController {
+  constructor(private productsService: ProductsService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'List of Products' })
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    // return {
+    //   message: `products limits=> ${limit} offset=> ${offset} brand=> ${brand}`,
+    // };
+    return this.productsService.findAll();
+  }
+
+  @Get('/filter')
+  getProductsFilter() {
+    return `yo soy filter`;
+  }
+
+  @Get('/:productId')
+  @HttpCode(HttpStatus.ACCEPTED)
+  getProduct(@Param('productId', ParseIntPipe) productId: number) {
+    // response.status(200).send({
+    //   message: `product ${productId}`,
+    // });
+    return this.productsService.findOne(productId);
+  }
+
+  @Post()
+  create(@Body() payload: CreateProductDto) {
+    // return {
+    //   message: 'accion de crear',
+    //   payload,
+    // };
+    return this.productsService.create(payload);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: UpdateProductDto) {
+    return this.productsService.update(+id, payload);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.productsService.remove(+id);
+  }
+}
